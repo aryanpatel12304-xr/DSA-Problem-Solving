8e92b010fbd72cae6fc07a672f8814d263b05f11
aryanpatel12304-xr <aryanpatel12304@gmail.com>
2026-08-28 21:20:04 +0530

    Time: 0 ms (100%), Space: 7.9 MB (83.07%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n = s.length();
+        for(int i = 0 ; i<n ; i++){
+            for(int j = n-1 ; j>0 ; j-- ){
+                swap(s[j] , s[j-1]);
+            }
+            if(s==goal){
+                return true;
+            }
+        }
+        return false;
+    }
+};
